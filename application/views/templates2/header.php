7e63fccacb533eb039936e4368d7bd94782a135f
Yogie Setya Nugraha <yogiesetyanugraha@gmail.com>
2022-10-23 21:23:25 +0700

    Setup Jurnal

--- application/views/templates2/header.php
+++ application/views/templates2/header.php
@@ -21,14 +21,16 @@
 
 	<!-- Custom styles for this page -->
     <link href=" <?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.css');?>" rel="stylesheet">
-
+  <!-- Select2 -->
+  <link rel="stylesheet" href="<?= base_url('assets/css/select2.min.css')?>">
+  <link rel="stylesheet" href="<?= base_url('assets/css/select2-bootstrap4.min.css')?>">
   <!-- Custom styles for this template-->
   <link href="<?= base_url('assets/css/sb-admin-2.min.css');?>" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" integrity="sha512-mSYUmp1HYZDFaVKK//63EcZq4iFWFjxSL+Z3T/aCt4IO9Cejm03q3NKKYN6pFQzY0SBOr8h+eCIAZHPXcpZaNw==" crossorigin="anonymous" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   
   <script src="<?= base_url('assets/vendor/datatables/jquery.dataTables.min.js');?>"></script>
-    <script src="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js');?>"></script>
+  <script src="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js');?>"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js" integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ==" crossorigin="anonymous"></script>
 
   <script>
