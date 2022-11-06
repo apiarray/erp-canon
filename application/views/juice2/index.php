d955366e76b85809df0c1865dc15a748188ecdf2
DenyAfriyan <90840905+DenyAfriyan@users.noreply.github.com>
2022-11-06 17:50:34 +0700

    Update index.php

--- application/views/juice2/index.php
+++ application/views/juice2/index.php
@@ -291,7 +291,7 @@
                         0
                         </td>
                         <td width="">
-                        ${results[i].total}
+                        ${formatRupiah(results[i].total)}
                         </td>
                         <td style="text-align:center">
                         <a onclick="getDataById(${results[i].id})" class="btn btn-success text-white" style=""><i class="fa fa-edit"></i>Edit</i></a>
@@ -362,4 +362,21 @@
             }
         });
     }
+     /* Fungsi formatRupiah */
+		function formatRupiah(angka, prefix){
+			var number_string = angka.replace(/[^,\d]/g, '').toString(),
+			split   		= number_string.split(','),
+			sisa     		= split[0].length % 3,
+			rupiah     		= split[0].substr(0, sisa),
+			ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
+ 
+			// tambahkan titik jika yang di input sudah menjadi angka ribuan
+			if(ribuan){
+				separator = sisa ? '.' : '';
+				rupiah += separator + ribuan.join('.');
+			}
+ 
+			rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+			return prefix == undefined ? rupiah : (rupiah ? rupiah : '');
+		}
 </script>
